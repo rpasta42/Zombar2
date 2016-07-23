a62386de412803514c5b2c3f0585a8f3a5c6c77f
Konstantin Kowalski <kostelkow@gmail.com>
2016-07-22 22:35:05 -0400

    Attempting to do android permission request.

--- Assets/GyroController.cs
+++ Assets/GyroController.cs
@@ -10,15 +10,14 @@ public class GyroController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-      var accelRot = new Vector3(Input.acceleration.y*50.0f + 20.0f,
+      var accelRot = new Vector3(Input.acceleration.y*50.0f + 30.0f,
                                  -Input.acceleration.x*50.0f,
-                                 0.0f);//-Input.acceleration.z*50.0f);
+                                 0.0f);
 
 
       var x = Mathf.LerpAngle(transform.eulerAngles.x, accelRot.x, 0.1f);
       var y = Mathf.LerpAngle(transform.eulerAngles.y, accelRot.y, 0.1f);
-      //var z = Mathf.LerpAngle(transform.eulerAngles.z, accelRot.z, 0.1f);
 
-      transform.eulerAngles = new Vector3(x, y, transform.eulerAngles.z); //z
+      transform.eulerAngles = new Vector3(x, y, transform.eulerAngles.z);
 	}
 }
---
+++ Assets/LiveCamera.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LiveCamera : MonoBehaviour {
+
+   void getPermission() {
+      if (ContextCompat.checkSelfPermission(thisActivity,Manifest.permission.READ_CONTACTS)
+              != PackageManager.PERMISSION_GRANTED) {
+           if (ActivityCompat.shouldShowRequestPermissionRationale(thisActivity,
+                  Manifest.permission.READ_CONTACTS)) {
+
+              // Show an expanation to the user *asynchronously* -- don't block
+              // this thread waiting for the user's response! After the user
+              // sees the explanation, try again to request the permission.
+
+          } else {
+
+              // No explanation needed, we can request the permission.
+
+              ActivityCompat.requestPermissions(thisActivity,
+                      new String[]{Manifest.permission.READ_CONTACTS},
+                      MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+              // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+              // app-defined int constant. The callback method gets the
+              // result of the request.
+          }
+      }
+
+   }
+
+   // Use this for initialization
+   IEnumerator Start () {
+      /*yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+      if (!Application.HasUserAuthorization(UserAuthorization.WebCam))
+         yield return false;*/
+
+
+      WebCamDevice[] devices = WebCamTexture.devices;
+
+      string camToUse = null;
+      var gotBackCam = false;
+      for(int i = 0; i < devices.Length; i++) {
+         camToUse = devices[i].name;
+         Debug.Log("Camera: " + devices[i].name);
+         if (!devices[i].isFrontFacing) {
+            gotBackCam = true;
+            break;
+         }
+      }
+      if (camToUse == null)
+         Debug.LogError("No camera found");
+      if (gotBackCam == false)
+         Debug.LogWarning("Only found front camera");
+
+      GameObject plane = GameObject.Find("VidPlane");
+      WebCamTexture cam = new WebCamTexture(camToUse); //(480, 480); //WebCamTexture();
+      var m = plane.GetComponent<Renderer>().material;
+      m.mainTexture = cam;
+      var c = m.color;
+      m.color = new Color(c.r, c.g, c.b, 0.4f);
+      cam.Play();
+   }
+
+	// Update is called once per frame
+   void Update () {
+
+   }
+}
