0961ffbfce55a551d7579f770d01d60583f9a8f7
Anony Moose <kostelkow@gmail.com>
2017-07-18 14:01:54 -0400

    Update GyroController.cs

--- Assets/GyroController.cs
+++ Assets/GyroController.cs
@@ -7,23 +7,23 @@ public class GyroController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-      Input.location.Start();
-      Input.compass.enabled = true;
+		Input.location.Start();
+		Input.compass.enabled = true;
 	}
 
 	// Update is called once per frame
 	void Update () {
-      var accelRot = new Vector3(Input.acceleration.y*accelCamFactor + initialAngleStart,
-                                 -Input.acceleration.x*accelCamFactor, //+ initialAngleStart,
-                                 0.0f);
+		var accelRot = new Vector3(Input.acceleration.y*accelCamFactor + initialAngleStart,
+					   -Input.acceleration.x*accelCamFactor, //+ initialAngleStart,
+					   0.0f);
 
-      var x = Mathf.LerpAngle(transform.eulerAngles.x, accelRot.x, 0.1f);
-      var y = Mathf.LerpAngle(transform.eulerAngles.y, accelRot.y, 0.1f);
+        var x = Mathf.LerpAngle(transform.eulerAngles.x, accelRot.x, 0.1f);
+        var y = Mathf.LerpAngle(transform.eulerAngles.y, accelRot.y, 0.1f);
 
-      //TODO: good compass, just lerp it
-      /*//transform.eulerAngles = new Vector3(x, y, transform.eulerAngles.z);
-      var heading1 = -Input.compass.magneticHeading;
-      var heading2 = -Input.compass.trueHeading;
-      transform.rotation = Quaternion.Euler(0, heading2, 0);*/
+        //TODO: good compass, just lerp it
+        /*//transform.eulerAngles = new Vector3(x, y, transform.eulerAngles.z);
+        var heading1 = -Input.compass.magneticHeading;
+        var heading2 = -Input.compass.trueHeading;
+        transform.rotation = Quaternion.Euler(0, heading2, 0);*/
 	}
 }
