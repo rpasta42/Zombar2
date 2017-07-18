62c0de901b194a343cae271f06b8439c381f7b52
Anony Moose <kostelkow@gmail.com>
2017-07-18 13:57:56 -0400

    Update CompassSwag.cs

--- Assets/CompassSwag.cs
+++ Assets/CompassSwag.cs
@@ -2,34 +2,34 @@
 using System.Collections;
 
 public class CompassSwag : MonoBehaviour {
-   private int start = 0;
+    private int start = 0;
 
-	// Use this for initialization
-	void Start () {
-	}
+    // Use this for initialization
+    void Start () {
+    }
 
-	// Update is called once per frame
-	void Update () {
-      return;
-      if (start++ < 30) return;
-      start = 0;
+    // Update is called once per frame
+    void Update () {
+        return;
+        if (start++ < 30) return;
+        start = 0;
 
-      GameObject c = GameObject.CreatePrimitive(PrimitiveType.Cube);
-      Rigidbody rb = c.AddComponent<Rigidbody>();
+        GameObject c = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        Rigidbody rb = c.AddComponent<Rigidbody>();
 
-      var north = Input.compass.magneticHeading;
+        var north = Input.compass.magneticHeading;
 
 
-      //Vector3 forward = new Vector3(0, 1, 0);
-      //forward = Quaternion.AngleAxis(-north, Vector3.forward) * forward;
-      Vector3 forward = new Vector3(0, -north, 0);
+        //Vector3 forward = new Vector3(0, 1, 0);
+        //forward = Quaternion.AngleAxis(-north, Vector3.forward) * forward;
+        Vector3 forward = new Vector3(0, -north, 0);
 
-      rb.AddForce(forward);
-      //Quaternion.Euler(0, -north, 0);
-      /*//transform.eulerAngles = new Vector3(x, y, transform.eulerAngles.z);
-      var heading1 = -Input.compass.magneticHeading;
-      var heading2 = -Input.compass.trueHeading;
-      transform.rotation = Quaternion.Euler(0, heading2, 0);
-      */
-	}
+        rb.AddForce(forward);
+        //Quaternion.Euler(0, -north, 0);
+        /*//transform.eulerAngles = new Vector3(x, y, transform.eulerAngles.z);
+        var heading1 = -Input.compass.magneticHeading;
+        var heading2 = -Input.compass.trueHeading;
+        transform.rotation = Quaternion.Euler(0, heading2, 0);
+        */
+    }
 }
